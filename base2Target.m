f8ad68b98464aa1d8a369a84bfd3a748fb4c698f
cutlercollin99 <110416785+cutlercollin99@users.noreply.github.com>
2023-12-02 18:46:46 -0500

    April tag to QARM Base
    This function inputs the AprilTag location from the camera and the current joint angles (t1-t4). It outputs a transformation matrix of the base to the that AprilTag. ie where we want the end-effector to go. I haven't confirmed this is accurate.

---
+++ base2Target.m
@@ -0,0 +1,63 @@
+function T_0t = joint2Target(P_ct,jointAngles)
+% This takes in the joint angles of the robot and position of the target
+% with respect to the camera. The output is the transformation matrix of
+% the target with respect to the base. ie where we want the end-effector to
+% go.
+
+% Define individual angles
+t1 = jointAngles(1);
+t2 = jointAngles(2);
+t3 = jointAngles(3);
+t4 = jointAngles(4);
+
+T_0w = FwdKinQARM_wrist(t1,t2,t3,t4); % base to wrist
+T_wc = [0, 1, 0, 5;
+       -1, 0, 0, 0;
+        0, 0, 1, 0;
+        0, 0, 0, 1]; % wrist to camera
+T_ct = [eye(3),P_ct';0,0,0,1]; % camera to target
+T_0t = T_0w*T_wc*T_ct;
+
+end
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function T0_w = FwdKinQARM_wrist(t1,t2,t3,t4)
+%FwdKinQARM creates the transformation matrix for the base to the tool frame
+% Inputs angles in radians of each joint
+
+% QARM lengths specified in the Lab 6 coordinate system
+L1 = 0.14; %m
+L2 = 0.35; %m
+L3 = 0.05; %m
+L4 = 0.25; %m
+L5 = 0.15; %m
+
+% Angle between vertical and common perpendicular of joint 2&3
+beta = atan2d(L3,L2);
+
+% DH Table of QARM
+DH = [0,0,L1,t1;
+    -90,0,0,t2-90+beta;
+    0,sqrt(L2^2+L3^2),0,t3-beta;
+    -90,0,L4,t4;
+    0,0,L5,0];
+
+% Calculates Transformation matrix base to wrist frame.
+T0_w = eye(4);
+for i= 1:4;
+    T0_w = T0_w*HTM(DH(i,1),DH(i,2),DH(i,3),DH(i,4));
+end
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function homoTransferMatrix = HTM(alphai_1,ai_1,di,thetai)
+%homoTransform This function creates the Homogeneous Transformation Matrix
+%   The angles are in radians. Use deg2rad for conversion.
+homoTransferMatrix = [cos(thetai),-sin(thetai), 0,ai_1;
+    sin(thetai)*cos(alphai_1), cos(thetai)*cos(alphai_1),-sin(alphai_1),-sin(alphai_1)*di;
+    sin(thetai)*sin(alphai_1),cos(thetai)*sin(alphai_1),cos(alphai_1),cos(alphai_1)*di;
+    0,0,0,1];
+end
